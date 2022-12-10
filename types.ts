e96ee469695be76ed440d4251f0755e3649b6986
Nkasi-e <nkasijnr@gmail.com>
2022-12-10 22:07:46 +0100

    init commit

---
+++ types.ts
@@ -0,0 +1,18 @@
+// boolean
+let common: boolean = true
+
+// number
+let age: number = 54
+
+// string
+
+let myColor: string = 'Blue'
+
+myColor = '4'
+
+// Arrays
+// array of strings
+let pets: string[] = ['cat', 'dog', 'pi']
+
+// another way to declare array
+let pet2: Array<string> = ['breeds', 'fowl', 'dove']
---
+++ typescript.ts
@@ -0,0 +1,4 @@
+const sum = (a: number, b: number) =>  a + b;
+
+
+console.log(sum(3, 5))
