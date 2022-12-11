d52b6103f12e5545c0cf0cbaf438d3c47d970fb2
Nkasi-e <nkasijnr@gmail.com>
2022-12-11 15:45:58 +0100

    more data types

--- types.ts
+++ types.ts
@@ -1,3 +1,7 @@
+/**
+ * @types - types in typescript
+ * */ 
+
 // boolean
 let common: boolean = true
 
@@ -16,3 +20,58 @@ let pets: string[] = ['cat', 'dog', 'pi']
 
 // another way to declare array
 let pet2: Array<string> = ['breeds', 'fowl', 'dove']
+let pet3: Array<object> = [
+    {
+        name : 'John'
+    },
+    {
+        name: 'Doe'
+    }
+]
+
+// Object
+
+let wizard: object = {
+   a: 'People'
+}
+
+// null and undefined
+let mehn: undefined = undefined
+
+let noobie: null = null
+
+
+// Tuple
+let items: [string, number]
+items = ['Grapes', 10]
+
+// Enum - Enumerable
+enum size { small = 1, medium = 2, large = 3 }
+let sizeName: string = size[1]
+
+// Any
+let whatever: any = 'Meaning'
+whatever = 6
+whatever = true
+
+// void - quite common when working with fn
+const sing = (): void => {
+    console.log('void')
+}
+
+// never
+const error = (): never => {
+    throw Error('oops')
+}
+
+// interface
+// interface can be used really well with object
+
+interface RobotArmy {
+    count: number,
+    type: string,
+    magic: string
+}
+let fightRobotArmy = (robots: RobotArmy) => {
+    console.log('FIGHT')
+}
